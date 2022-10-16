39fe63b4deec074fe0deb04aab87d8ca83e4ebf2
Jason Shen <jason_2000_nz@hotmail.com>
2022-10-16 20:09:40 +1300

    fixed with updates of plugins

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -46,7 +46,7 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
       augroup lsp_document_highlight
@@ -81,12 +81,12 @@ local function lsp_keymaps(bufnr)
   )
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format()' ]]
 end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
@@ -98,8 +98,7 @@ local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
-M.capabilities = cmp_nvim_lsp.default_capabilities()
 -- M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
